23fb842f01c1b3f67ca84417dafdd839ed897c00
Ciaran OKeeffe <ciaranstudio@gmail.com>
2024-08-16 22:41:22 -0400

    first commit

---
+++ src/data/images.js
@@ -0,0 +1,52 @@
+const images = [
+  {
+    image:
+      'img/1.jpeg'
+  },
+  {
+    image:
+      'img/2.jpeg'
+  },
+  {
+    image:
+      'img/3.jpeg'
+  },
+  {
+    image:
+      'img/4.jpeg'
+  },
+  // {
+  //   image:
+  //     'img/5.jpeg'
+  // },
+  // {
+  //   image:
+  //     'img/6.jpeg'
+  // },
+  // {
+  //   image:
+  //     'img/7.jpeg'
+  // },
+  // {
+  //   image:
+  //     'img/8.jpeg'
+  // },
+  // {
+  //   image:
+  //     'img/9.jpeg'
+  // },
+  // {
+  //   image:
+  //     'img/10.jpeg'
+  // },
+  // {
+  //   image:
+  //     'img/11.jpeg'
+  // },
+  // {
+  //   image:
+  //     'img/12.jpeg'
+  // }
+]
+
+export default images
